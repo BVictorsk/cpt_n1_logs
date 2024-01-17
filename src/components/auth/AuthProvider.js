d1760e75bb479995e8190aeeb78310d0bcd196bf
Brian Victor De Mello Saka <brian_saka@hotmail.com>
2024-01-17 11:54:43 -0300

    added logins

--- src/components/auth/AuthProvider.js
+++ src/components/auth/AuthProvider.js
@@ -2,9 +2,14 @@
 
 export const authenticateUser = (login, password) => {
     const validUsers = [
-      { username: 'brian', login: '1010', senha: '1010' },
-      { username: 'luis', login: '1008', senha: '1008' },
-      { username: 'Daniel', login: '1002', senha: '1002' },
+      { username: 'Brian', login: '1010', senha: '1010' },
+      { username: 'Daniel', login: '1022', senha: '1022' },
+      { username: 'Edivaldo', login: '1014', senha: '1014' },
+      { username: 'Gabriel', login: '1023', senha: '1023' },
+      { username: 'Lucas', login: '1013', senha: '1013' },
+      { username: 'Luis', login: '1008', senha: '1008' },
+      { username: 'Jefferson', login: '1020', senha: '1020' },
+      { username: 'Renan', login: '1002', senha: '1002' },
     ];
   
     return validUsers.some(
